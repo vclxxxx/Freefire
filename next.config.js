4c4e3d92893f4ee6a35d6fed8875c7c82403060c
vclxxxx <hoanghuuviet2211@gmail.com>
2025-09-19 20:19:02 +0700

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Allow image domains if you want to use next/image (optional)
   images: {
     domains: ['profile.thug4ff.com'],
   },
